921adae24b43fd8ef4a6c1bcc0f84b389cfded4a
Kaung Htet Zaw <me@kaunghtet.xyz>
2019-06-04 04:03:08 +0800

    Initialize slate

--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useReducer, useState } from 'react';
 import styled from 'styled-components';
 import format from 'date-fns/format';
+import { Editor } from 'slate-react';
+import { Value } from 'slate';
+import { tsNamespaceExportDeclaration } from '@babel/types';
 
-const Wrapper = styled.div`
-`;
+const Wrapper = styled.div``;
 
-const H1 = styled.h1`
-`;
+const H1 = styled.h1``;
 
 const List = styled.ul`
   list-style: none;
@@ -14,13 +15,39 @@ const List = styled.ul`
   margin: 0;
 `;
 
-const Item = styled.li`
-`;
+const Item = styled.li``;
+
+const initialValue = Value.fromJSON({
+  document: {
+    nodes: [
+      {
+        object: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            object: 'text',
+            leaves: [{ text: '' }]
+          }
+        ]
+      }
+    ]
+  }
+});
 
 const Todo = () => {
+  const [value, setValue] = useState(initialValue);
   return (
     <Wrapper>
       <H1>{format(new Date(), 'MMM D')}</H1>
+      <Editor
+        value={value}
+        onChange={({ value }) => setValue(value)}
+        onKeyDown={(event, editor, next) => {
+          if (event.key !== 'Enter') return next();
+
+          event.preventDefault();
+        }}
+      />
       <List>
         <Item>Lorem ipsum foobar</Item>
         <Item>Lorem ipsum foobar</Item>
